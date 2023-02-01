1c489096f11d356c96866fb8498afc0774348697
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-01 18:03:31 +0800

    add const before callWs

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ calculateButton.addEventListener("click", function (event) {
   const width = parseFloat(widthInput.value);
   result.innerHTML = length * width;
 });
+
+const callWs = function () {
+  // The Endpoint URL
+  const url = "https://jsonplaceholder.typicode.com/posts/1";
+  fetch(url)
+    .then(function (response) {
+      // Render the Response Status
+      document.getElementById("result").innerHTML = response.status;
+      // Parse the body as JSON
+      return response.json();
+    })
+    .then(function (json) {
+      // Render the parsed body
+      document.getElementById("result_json").innerHTML = JSON.stringify(json);
+    });
+};
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post("/calculate-area", (req, res) => {
+  const length = parseFloat(req.body.length);
+  const width = parseFloat(req.body.width);
+  const area = length * width;
+  res.json({ area });
+});
+
+app.listen(port, () => {
+  console.log(
+    `Grass Field Area Calculator API is listening at http://localhost:${port}`
+  );
+});
